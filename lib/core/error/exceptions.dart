f9ce3b76ae2a93a394174a24b0c91b61de997e12
Ganney Dortch <ganneyd@gmail.com>
2024-01-21 23:59:21 +0300

    initial commit

---
+++ lib/core/error/exceptions.dart
@@ -0,0 +1,18 @@
+///Thrown when an error occurs when interacting with the database
+class DatabaseException implements Exception {}
+
+///Thrown when an error occurs when creating new data
+///in the database
+class CreateDataException implements Exception {}
+
+///Thrown when an error occurs when updating data
+///in the database
+class UpdateDataException implements Exception {}
+
+///Thrown when an error occurs when reading data
+///from the database
+class ReadDataException implements Exception {}
+
+///Thrown when an error occurs when deleting data
+///from the database
+class DeleteDataException implements Exception {}
