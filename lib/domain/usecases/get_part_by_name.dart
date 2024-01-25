94bd3d23d579e8955891c42d96e26470abda000a
Ganney Dortch <ganneyd@gmail.com>
2024-01-25 03:48:34 +0300

    implement usecases for part retrieval

---
+++ lib/domain/usecases/get_part_by_name.dart
@@ -0,0 +1,30 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:inventory_v1/core/error/failures.dart';
+import 'package:inventory_v1/core/usecases/usecases.dart';
+import 'package:inventory_v1/domain/entities/part/part_entity.dart';
+import 'package:inventory_v1/domain/repositories/part_repository.dart';
+
+class GetPartByNameUseCase implements UseCase<List<PartEntity>, Params> {
+  const GetPartByNameUseCase(PartRepository partRepository)
+      : _partRepository = partRepository;
+
+  final PartRepository _partRepository;
+  @override
+  Future<Either<Failure, List<PartEntity>>> call(Params params) async {
+    Either<Failure, List<PartEntity>> usecase =
+        await _partRepository.searchPartsByField(
+            fieldName: PartRepository.partNameField, queryKey: params.queryKey);
+
+    return usecase.fold(
+        (l) => const Left<Failure, List<PartEntity>>(ReadDataFailure()),
+        (List<PartEntity> parts) => Right<Failure, List<PartEntity>>(parts));
+  }
+}
+
+class Params extends Equatable {
+  const Params({required this.queryKey});
+  final String queryKey;
+  @override
+  List<Object?> get props => <Object>[queryKey];
+}
---
+++ lib/domain/usecases/get_part_by_part_number.dart
@@ -0,0 +1,31 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:inventory_v1/core/error/failures.dart';
+import 'package:inventory_v1/core/usecases/usecases.dart';
+import 'package:inventory_v1/domain/entities/part/part_entity.dart';
+import 'package:inventory_v1/domain/repositories/part_repository.dart';
+
+class GetPartByPartNumberUseCase implements UseCase<List<PartEntity>, Params> {
+  const GetPartByPartNumberUseCase(PartRepository partRepository)
+      : _partRepository = partRepository;
+
+  final PartRepository _partRepository;
+  @override
+  Future<Either<Failure, List<PartEntity>>> call(Params params) async {
+    Either<Failure, List<PartEntity>> usecase =
+        await _partRepository.searchPartsByField(
+            fieldName: PartRepository.partNumberField,
+            queryKey: params.queryKey);
+
+    return usecase.fold(
+        (l) => const Left<Failure, List<PartEntity>>(ReadDataFailure()),
+        (List<PartEntity> parts) => Right<Failure, List<PartEntity>>(parts));
+  }
+}
+
+class Params extends Equatable {
+  const Params({required this.queryKey});
+  final String queryKey;
+  @override
+  List<Object?> get props => <Object>[queryKey];
+}
