45df8f837a64822b740886765a72ae8db0bf3d19
Ganney Dortch <ganneyd@gmail.com>
2024-01-30 03:39:12 +0300

    created insertion point for application

---
+++ lib/app.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:inventory_v1/presentation/cubit/dependency_check_cubit.dart';
+import 'package:inventory_v1/presentation/cubit/dependency_check_state.dart';
+import 'package:inventory_v1/route_generator.dart';
+
+class App extends StatelessWidget {
+  const App() : super(key: const Key('app-view'));
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Inventory',
+      home: BlocProvider<DependencyCheckCubit>(
+        create: (_) => DependencyCheckCubit(),
+        child: BlocBuilder<DependencyCheckCubit, DependencyCheckState>(
+          builder: (context, state) {
+            WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+              if (state.dependencyCheckStateStatus ==
+                  DependencyCheckStateStatus.loadedSuccessfully) {
+                Navigator.of(context).pushNamed('/home_page');
+              }
+            });
+
+            return Scaffold(
+              body: state.dependencyCheckStateStatus ==
+                      DependencyCheckStateStatus.loadedUnsuccessfully
+                  ? Text(state.error != null ? state.error! : ' ')
+                  : const CircularProgressIndicator(),
+            );
+          },
+        ),
+      ),
+      onGenerateRoute: RouteGenerator.generateRoute,
+    );
+  }
+}
