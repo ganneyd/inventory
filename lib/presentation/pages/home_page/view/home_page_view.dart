6e1e5cb31bd05cb08b3912205dfcbf06f157be51
Ganney Dortch <ganneyd@gmail.com>
2024-01-30 03:42:34 +0300

    created view for home page

--- lib/presentation/pages/home_page/view/home_page_view.dart
+++ lib/presentation/pages/home_page/view/home_page_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:inventory_v1/data/repositories/part_repository_implementation.dart';
 import 'package:inventory_v1/presentation/pages/home_page/cubit/home_page_cubit.dart';
 import 'package:inventory_v1/presentation/pages/home_page/cubit/home_page_state.dart';
 import 'package:inventory_v1/presentation/widgets/widget_bucket.dart';
+import 'package:inventory_v1/service_locator.dart';
 
 class HomePageView extends StatelessWidget {
   HomePageView()
@@ -12,27 +14,43 @@ class HomePageView extends StatelessWidget {
 
   void searchCallback() {}
 
-  TextEditingController controller = TextEditingController(text: 'search');
+  final TextEditingController controller = TextEditingController(text: '');
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<HomePageCubit, HomePageState>(
-      builder: (context, state) {
-        return Scaffold(
-          key: _scaffoldKey,
-          body: Column(children: [
-            CustomSearchBar(
-              controller: controller,
-              onPressed: searchCallback,
-            ),
-            Row(
-              children: [
-                LargeButton(buttonName: 'Add Part', onPressed: () {}),
-                LargeButton(buttonName: 'Manage Inventory', onPressed: () {}),
-              ],
-            )
-          ]),
-        );
-      },
+    return BlocProvider<HomePageCubit>(
+      create: (_) => HomePageCubit(
+          partRepository: locator<PartRepositoryImplementation>()),
+      child: BlocBuilder<HomePageCubit, HomePageState>(
+        builder: (context, state) {
+          return Scaffold(
+            key: _scaffoldKey,
+            body: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  CustomSearchBar(
+                    textFieldKey: const Key('home-page-search-bar'),
+                    controller: controller,
+                    onPressed: searchCallback,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      LargeButton(
+                          key: const Key('add-part-btn'),
+                          buttonName: 'Add Part',
+                          onPressed: () =>
+                              Navigator.of(context).pushNamed('/add_part')),
+                      LargeButton(
+                          key: const Key('manage-inv-btn'),
+                          buttonName: 'Manage Inventory',
+                          onPressed: () => Navigator.of(context)
+                              .pushNamed('/manage_inventory')),
+                    ],
+                  )
+                ]),
+          );
+        },
+      ),
     );
   }
 }
