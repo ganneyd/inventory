d0dc657705a71da6b7d78edabd27449fa95972b4
Ganney Dortch <ganneyd@gmail.com>
2024-01-30 03:42:20 +0300

    service locator bug fixed, now all instances are instantiated correctly

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,52 +1,71 @@
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:inventory_v1/data/datasources/local_database.dart';
-import 'package:hive/hive.dart';
 import 'package:inventory_v1/data/repositories/part_repository_implementation.dart';
-import 'package:path_provider/path_provider.dart' as path_provider;
+import 'package:logging/logging.dart';
 import 'package:inventory_v1/domain/usecases/usecases_bucket.dart';
 
 final GetIt locator = GetIt.instance;
-
+Logger serviceLocatorLogger = Logger('service_locator');
 Future<void> initDependencies() async {
+  serviceLocatorLogger = Logger('service_logga');
+  WidgetsFlutterBinding.ensureInitialized();
   await initHive();
-  setupLocator();
+  serviceLocatorLogger.finest('initializing service locator');
+  await setupLocator();
 }
 
 Future<void> initHive() async {
-  final appDocumentDirectory =
-      await path_provider.getApplicationDocumentsDirectory();
-  Hive.init(appDocumentDirectory.path);
+  serviceLocatorLogger.finest('initializing HIVE');
+  // Directory appDocumentDirectory;
+  // try {
+  //   appDocumentDirectory =
+  //       await path_provider.getApplicationDocumentsDirectory();
+  //   serviceLocatorLogger.finest('got application path for hive');
+  // } catch (e) {
+  //   serviceLocatorLogger
+  //       .warning('exception getting application path  getting temp ...$e  ');
+  //   appDocumentDirectory = await path_provider.getTemporaryDirectory();
+  //   serviceLocatorLogger.finest('got temp path');
+  // }
+  await Hive.initFlutter();
+  await Hive.openBox<Map<String, dynamic>>('parts');
+  serviceLocatorLogger.finest('initialized hive');
 }
 
 //Initialize the service locator
 
 Future<void> setupLocator() async {
 //! Datasource
-  locator.registerLazySingletonAsync<Box<Map<String, dynamic>>>(() async {
-    final box = await Hive.openBox<Map<String, dynamic>>('parts');
-    return box;
-  });
+
+  locator.registerLazySingletonAsync<Box<Map<String, dynamic>>>(
+      () async => await Hive.openBox<Map<String, dynamic>>('parts'));
+  serviceLocatorLogger.finest('opened hive box');
+
   //!Data Layer
   //!Repositories
   locator.registerLazySingleton<LocalDataSourceImplementation>(() =>
-      LocalDataSourceImplementation(locator<Box<Map<String, dynamic>>>()));
+      LocalDataSourceImplementation(Hive.box<Map<String, dynamic>>('parts')));
   locator.registerLazySingleton<PartRepositoryImplementation>(() =>
       PartRepositoryImplementation(locator<LocalDataSourceImplementation>()));
   //!Usecases
-  locator.registerFactory<AddPartUsecase>(() => AddPartUsecase(locator()));
-  locator
-      .registerFactory<DeletePartUsecase>(() => DeletePartUsecase(locator()));
-  locator.registerFactory<EditPartUsecase>(() => EditPartUsecase(locator()));
-  locator
-      .registerFactory<GetAllPartsUsecase>(() => GetAllPartsUsecase(locator()));
+  locator.registerFactory<AddPartUsecase>(
+      () => AddPartUsecase(locator<PartRepositoryImplementation>()));
+  locator.registerFactory<DeletePartUsecase>(
+      () => DeletePartUsecase(locator<PartRepositoryImplementation>()));
+  locator.registerFactory<EditPartUsecase>(
+      () => EditPartUsecase(locator<PartRepositoryImplementation>()));
+  locator.registerFactory<GetAllPartsUsecase>(
+      () => GetAllPartsUsecase(locator<PartRepositoryImplementation>()));
   locator.registerFactory<GetPartByNameUseCase>(
-      () => GetPartByNameUseCase(locator()));
+      () => GetPartByNameUseCase(locator<PartRepositoryImplementation>()));
   locator.registerFactory<GetPartByNsnUseCase>(
-      () => GetPartByNsnUseCase(locator()));
-  locator.registerFactory<GetPartByPartNumberUsecase>(
-      () => GetPartByPartNumberUsecase(locator()));
-  locator.registerFactory<GetPartBySerialNumberUsecase>(
-      () => GetPartBySerialNumberUsecase(locator()));
+      () => GetPartByNsnUseCase(locator<PartRepositoryImplementation>()));
+  locator.registerFactory<GetPartByPartNumberUsecase>(() =>
+      GetPartByPartNumberUsecase(locator<PartRepositoryImplementation>()));
+  locator.registerFactory<GetPartBySerialNumberUsecase>(() =>
+      GetPartBySerialNumberUsecase(locator<PartRepositoryImplementation>()));
   //!Presentation Layer
 //!Pages
 }
