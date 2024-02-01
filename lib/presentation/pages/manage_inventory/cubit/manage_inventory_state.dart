db3e9ccf2df6f10abd61da8aa782cfd16f6b7c4a
Ganney Dortch <ganneyd@gmail.com>
2024-02-02 02:21:21 +0300

    added state control to manage inventory page

--- lib/presentation/pages/manage_inventory/cubit/manage_inventory_state.dart
+++ lib/presentation/pages/manage_inventory/cubit/manage_inventory_state.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:inventory_v1/data/models/part/part_model.dart';
+import 'package:inventory_v1/domain/models/part/part_model.dart';
 
 part 'manage_inventory_state.freezed.dart';
 
@@ -9,9 +10,6 @@ enum ManageInventoryStateStatus {
   loading,
   loadedSuccessfully,
   loadedUnsuccessfully,
-  creatingData,
-  createdDataSuccessfully,
-  createdDataUnsuccessfully,
   fetchingData,
   fetchedDataSuccessfully,
   fetchedDataUnsuccessfully
@@ -21,9 +19,9 @@ enum ManageInventoryStateStatus {
 class ManageInventoryState with _$ManageInventoryState {
   factory ManageInventoryState({
     required ScrollController scrollController,
-    @Default(<Part>[]) List<Part> part,
-    String? error,
+    @Default(<Part>[]) List<Part> parts,
+    @Default('no error') error,
     @Default(ManageInventoryStateStatus.loading)
-    ManageInventoryStateStatus manageInventoryStateStatus,
+    ManageInventoryStateStatus status,
   }) = _ManageInventoryState;
 }
--- lib/presentation/pages/manage_inventory/cubit/manage_inventory_state.freezed.dart
+++ lib/presentation/pages/manage_inventory/cubit/manage_inventory_state.freezed.dart
@@ -17,10 +17,16 @@ final _privateConstructorUsedError = UnsupportedError(
 /// @nodoc
 mixin _$ManageInventoryState {
   ScrollController get scrollController => throw _privateConstructorUsedError;
+<<<<<<< HEAD
   List<Part> get part => throw _privateConstructorUsedError;
   String? get error => throw _privateConstructorUsedError;
   ManageInventoryStateStatus get manageInventoryStateStatus =>
       throw _privateConstructorUsedError;
+=======
+  List<Part> get parts => throw _privateConstructorUsedError;
+  dynamic get error => throw _privateConstructorUsedError;
+  ManageInventoryStateStatus get status => throw _privateConstructorUsedError;
+>>>>>>> 9594ee4 (added state control to manage inventory page)
 
   @JsonKey(ignore: true)
   $ManageInventoryStateCopyWith<ManageInventoryState> get copyWith =>
@@ -35,9 +41,15 @@ abstract class $ManageInventoryStateCopyWith<$Res> {
   @useResult
   $Res call(
       {ScrollController scrollController,
+<<<<<<< HEAD
       List<Part> part,
       String? error,
       ManageInventoryStateStatus manageInventoryStateStatus});
+=======
+      List<Part> parts,
+      dynamic error,
+      ManageInventoryStateStatus status});
+>>>>>>> 9594ee4 (added state control to manage inventory page)
 }
 
 /// @nodoc
@@ -55,26 +67,36 @@ class _$ManageInventoryStateCopyWithImpl<$Res,
   @override
   $Res call({
     Object? scrollController = null,
+<<<<<<< HEAD
     Object? part = null,
+=======
+    Object? parts = null,
+>>>>>>> 9594ee4 (added state control to manage inventory page)
     Object? error = freezed,
-    Object? manageInventoryStateStatus = null,
+    Object? status = null,
   }) {
     return _then(_value.copyWith(
       scrollController: null == scrollController
           ? _value.scrollController
           : scrollController // ignore: cast_nullable_to_non_nullable
               as ScrollController,
+<<<<<<< HEAD
       part: null == part
           ? _value.part
           : part // ignore: cast_nullable_to_non_nullable
+=======
+      parts: null == parts
+          ? _value.parts
+          : parts // ignore: cast_nullable_to_non_nullable
+>>>>>>> 9594ee4 (added state control to manage inventory page)
               as List<Part>,
       error: freezed == error
           ? _value.error
           : error // ignore: cast_nullable_to_non_nullable
-              as String?,
-      manageInventoryStateStatus: null == manageInventoryStateStatus
-          ? _value.manageInventoryStateStatus
-          : manageInventoryStateStatus // ignore: cast_nullable_to_non_nullable
+              as dynamic,
+      status: null == status
+          ? _value.status
+          : status // ignore: cast_nullable_to_non_nullable
               as ManageInventoryStateStatus,
     ) as $Val);
   }
@@ -90,9 +112,15 @@ abstract class _$$ManageInventoryStateImplCopyWith<$Res>
   @useResult
   $Res call(
       {ScrollController scrollController,
+<<<<<<< HEAD
       List<Part> part,
       String? error,
       ManageInventoryStateStatus manageInventoryStateStatus});
+=======
+      List<Part> parts,
+      dynamic error,
+      ManageInventoryStateStatus status});
+>>>>>>> 9594ee4 (added state control to manage inventory page)
 }
 
 /// @nodoc
@@ -107,15 +135,20 @@ class __$$ManageInventoryStateImplCopyWithImpl<$Res>
   @override
   $Res call({
     Object? scrollController = null,
+<<<<<<< HEAD
     Object? part = null,
+=======
+    Object? parts = null,
+>>>>>>> 9594ee4 (added state control to manage inventory page)
     Object? error = freezed,
-    Object? manageInventoryStateStatus = null,
+    Object? status = null,
   }) {
     return _then(_$ManageInventoryStateImpl(
       scrollController: null == scrollController
           ? _value.scrollController
           : scrollController // ignore: cast_nullable_to_non_nullable
               as ScrollController,
+<<<<<<< HEAD
       part: null == part
           ? _value._part
           : part // ignore: cast_nullable_to_non_nullable
@@ -127,6 +160,16 @@ class __$$ManageInventoryStateImplCopyWithImpl<$Res>
       manageInventoryStateStatus: null == manageInventoryStateStatus
           ? _value.manageInventoryStateStatus
           : manageInventoryStateStatus // ignore: cast_nullable_to_non_nullable
+=======
+      parts: null == parts
+          ? _value._parts
+          : parts // ignore: cast_nullable_to_non_nullable
+              as List<Part>,
+      error: freezed == error ? _value.error! : error,
+      status: null == status
+          ? _value.status
+          : status // ignore: cast_nullable_to_non_nullable
+>>>>>>> 9594ee4 (added state control to manage inventory page)
               as ManageInventoryStateStatus,
     ));
   }
@@ -137,6 +180,7 @@ class __$$ManageInventoryStateImplCopyWithImpl<$Res>
 class _$ManageInventoryStateImpl implements _ManageInventoryState {
   _$ManageInventoryStateImpl(
       {required this.scrollController,
+<<<<<<< HEAD
       final List<Part> part = const <Part>[],
       this.error,
       this.manageInventoryStateStatus = ManageInventoryStateStatus.loading})
@@ -155,13 +199,38 @@ class _$ManageInventoryStateImpl implements _ManageInventoryState {
 
   @override
   final String? error;
+=======
+      final List<Part> parts = const <Part>[],
+      this.error = 'no error',
+      this.status = ManageInventoryStateStatus.loading})
+      : _parts = parts;
+
+  @override
+  final ScrollController scrollController;
+  final List<Part> _parts;
+>>>>>>> 9594ee4 (added state control to manage inventory page)
   @override
   @JsonKey()
-  final ManageInventoryStateStatus manageInventoryStateStatus;
+  List<Part> get parts {
+    if (_parts is EqualUnmodifiableListView) return _parts;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_parts);
+  }
+
+  @override
+  @JsonKey()
+  final dynamic error;
+  @override
+  @JsonKey()
+  final ManageInventoryStateStatus status;
 
   @override
   String toString() {
+<<<<<<< HEAD
     return 'ManageInventoryState(scrollController: $scrollController, part: $part, error: $error, manageInventoryStateStatus: $manageInventoryStateStatus)';
+=======
+    return 'ManageInventoryState(scrollController: $scrollController, parts: $parts, error: $error, status: $status)';
+>>>>>>> 9594ee4 (added state control to manage inventory page)
   }
 
   @override
@@ -171,21 +240,33 @@ class _$ManageInventoryStateImpl implements _ManageInventoryState {
             other is _$ManageInventoryStateImpl &&
             (identical(other.scrollController, scrollController) ||
                 other.scrollController == scrollController) &&
+<<<<<<< HEAD
             const DeepCollectionEquality().equals(other._part, _part) &&
             (identical(other.error, error) || other.error == error) &&
             (identical(other.manageInventoryStateStatus,
                     manageInventoryStateStatus) ||
                 other.manageInventoryStateStatus ==
                     manageInventoryStateStatus));
+=======
+            const DeepCollectionEquality().equals(other._parts, _parts) &&
+            const DeepCollectionEquality().equals(other.error, error) &&
+            (identical(other.status, status) || other.status == status));
+>>>>>>> 9594ee4 (added state control to manage inventory page)
   }
 
   @override
   int get hashCode => Object.hash(
       runtimeType,
       scrollController,
+<<<<<<< HEAD
       const DeepCollectionEquality().hash(_part),
       error,
       manageInventoryStateStatus);
+=======
+      const DeepCollectionEquality().hash(_parts),
+      const DeepCollectionEquality().hash(error),
+      status);
+>>>>>>> 9594ee4 (added state control to manage inventory page)
 
   @JsonKey(ignore: true)
   @override
@@ -198,6 +279,7 @@ class _$ManageInventoryStateImpl implements _ManageInventoryState {
 
 abstract class _ManageInventoryState implements ManageInventoryState {
   factory _ManageInventoryState(
+<<<<<<< HEAD
           {required final ScrollController scrollController,
           final List<Part> part,
           final String? error,
@@ -208,10 +290,21 @@ abstract class _ManageInventoryState implements ManageInventoryState {
   ScrollController get scrollController;
   @override
   List<Part> get part;
+=======
+      {required final ScrollController scrollController,
+      final List<Part> parts,
+      final dynamic error,
+      final ManageInventoryStateStatus status}) = _$ManageInventoryStateImpl;
+
   @override
-  String? get error;
+  ScrollController get scrollController;
+>>>>>>> 9594ee4 (added state control to manage inventory page)
   @override
-  ManageInventoryStateStatus get manageInventoryStateStatus;
+  List<Part> get parts;
+  @override
+  dynamic get error;
+  @override
+  ManageInventoryStateStatus get status;
   @override
   @JsonKey(ignore: true)
   _$$ManageInventoryStateImplCopyWith<_$ManageInventoryStateImpl>
