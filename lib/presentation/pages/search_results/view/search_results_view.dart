6e3294ffe5364ad4bf2e9b454d63f410686defb2
Ganney Dortch <ganneyd@gmail.com>
2024-02-02 02:25:56 +0300

    create UI for search results page

---
+++ lib/presentation/pages/search_results/view/search_results_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SearchResults extends StatelessWidget {
+  const SearchResults() : super(key: const Key('search-results-view'));
+
+  @override
+  Widget build(Object context) {
+    return const Center(child: CircularProgressIndicator());
+  }
+}
