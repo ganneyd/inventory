9350cc953d12d0a112e6b8ba552cf470a7d76df1
Ganney Dortch <ganneyd@gmail.com>
2024-02-02 02:26:13 +0300

    create state management for search results page

--- lib/presentation/pages/search_results/cubit/search_results_state.dart
+++ lib/presentation/pages/search_results/cubit/search_results_state.dart
@@ -1,24 +1,23 @@
+import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:inventory_v1/data/models/part/part_model.dart';
+import 'package:inventory_v1/domain/models/part/part_model.dart';
 
 part 'search_results_state.freezed.dart';
 
 //the different states the add_part page can be in
 enum SearchResultsStateStatus {
   loading,
+  searching,
   loadedSuccessfully,
   loadedUnsuccessfully,
-  creatingData,
-  createdDataSuccessfully,
-  createdDataUnsuccessfully
 }
 
 @freezed
 class SearchResultsState with _$SearchResultsState {
   factory SearchResultsState({
-    Part? part,
-    String? error,
-    @Default(SearchResultsStateStatus.loading)
-    SearchResultsStateStatus searchResultsStateStatus,
+    required ScrollController scrollController,
+    @Default(<Part>[]) List<Part> parts,
+    @Default('no-error') String error,
+    @Default(SearchResultsStateStatus.loading) SearchResultsStateStatus status,
   }) = _SearchResultsState;
 }
