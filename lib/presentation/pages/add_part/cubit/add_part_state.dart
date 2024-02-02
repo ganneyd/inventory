b8a9ac118008e153e8a08df87dc9c373228ff989
Ganney Dortch <ganneyd@gmail.com>
2024-02-02 21:03:05 +0300

    added form key, form validation flag, refactor unit of issue controller to a enum variable

--- lib/presentation/pages/add_part/cubit/add_part_state.dart
+++ lib/presentation/pages/add_part/cubit/add_part_state.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:inventory_v1/core/util/util.dart';
 import 'package:inventory_v1/domain/models/part/part_model.dart';
 
 part 'add_part_state.freezed.dart';
 
-//the different states the add_part page can be in
+//the different states` the add_part page can be in
 enum AddPartStateStatus {
   loading,
   loadedSuccessfully,
@@ -19,12 +20,14 @@ class AddPartState with _$AddPartState {
   factory AddPartState({
     Part? part,
     String? error,
+    required GlobalKey<FormState> formKey,
+    @Default(false) bool isFormValid,
+    @Default(UnitOfIssue.EA) UnitOfIssue unitOfIssue,
     required TextEditingController nsnController,
     required TextEditingController partNumberController,
     required TextEditingController serialNumberController,
     required TextEditingController nomenclatureController,
     required TextEditingController locationController,
-    required TextEditingController unitOfIssueController,
     required TextEditingController quantityController,
     required TextEditingController requisitionPointController,
     required TextEditingController requisitionQuantityController,
