e70094167811ae8c986c585488c63e9c58160e0a
Ganney Dortch <ganneyd@gmail.com>
2024-02-02 21:06:31 +0300

    added disable flag to button

--- lib/presentation/widgets/buttons/small_button_widget.dart
+++ lib/presentation/widgets/buttons/small_button_widget.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
 class SmallButton extends StatelessWidget {
+  final bool isDisabled;
   final String buttonName;
   final VoidCallback onPressed;
   const SmallButton(
-      {super.key, required this.buttonName, required this.onPressed});
+      {super.key,
+      required this.buttonName,
+      required this.onPressed,
+      this.isDisabled = false});
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -13,7 +17,7 @@ class SmallButton extends StatelessWidget {
           style: ElevatedButton.styleFrom(
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(0.0))),
-          onPressed: onPressed,
+          onPressed: isDisabled ? null : onPressed,
           child: Padding(
             padding: const EdgeInsetsDirectional.all(10),
             child: Text(buttonName),
