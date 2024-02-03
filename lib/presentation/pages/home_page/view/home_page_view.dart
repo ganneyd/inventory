2791285b2f2f6edd594678ea29ff347bb08cd431
Ganney Dortch <ganneyd@gmail.com>
2024-02-03 04:01:50 +0300

    removed unnecessary part repo variable

--- lib/presentation/pages/home_page/view/home_page_view.dart
+++ lib/presentation/pages/home_page/view/home_page_view.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:inventory_v1/data/repositories/part_repository_implementation.dart';
 import 'package:inventory_v1/presentation/pages/home_page/cubit/home_page_cubit.dart';
 import 'package:inventory_v1/presentation/pages/home_page/cubit/home_page_state.dart';
 import 'package:inventory_v1/presentation/widgets/widget_bucket.dart';
-import 'package:inventory_v1/service_locator.dart';
 
 class HomePageView extends StatelessWidget {
   HomePageView()
@@ -18,8 +16,7 @@ class HomePageView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider<HomePageCubit>(
-      create: (_) => HomePageCubit(
-          partRepository: locator<PartRepositoryImplementation>()),
+      create: (_) => HomePageCubit(),
       child: BlocBuilder<HomePageCubit, HomePageState>(
         builder: (context, state) {
           return Scaffold(
