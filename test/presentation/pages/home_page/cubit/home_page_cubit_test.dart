b7d33dd14d7af5bbd1dfccfc2226ed6e41ad10f0
Ganney Dortch <ganneyd@gmail.com>
2024-02-05 02:38:42 +0300

    removed unnecessary imports

--- test/presentation/pages/home_page/cubit/home_page_cubit_test.dart
+++ test/presentation/pages/home_page/cubit/home_page_cubit_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:inventory_v1/presentation/pages/home_page/cubit/home_page_cubit.dart';
 import 'package:inventory_v1/presentation/pages/home_page/cubit/home_page_state.dart';
-import 'package:mocktail/mocktail.dart';
 
 void main() {
   late HomePageCubit homePageCubit;
