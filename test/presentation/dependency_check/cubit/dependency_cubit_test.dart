0b230a7f34a2e68bee4693e0e30403d8d15ca508
Ganney Dortch <ganneyd@gmail.com>
2024-02-05 02:38:42 +0300

    removed unnecessary imports

--- test/presentation/dependency_check/cubit/dependency_cubit_test.dart
+++ test/presentation/dependency_check/cubit/dependency_cubit_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:inventory_v1/presentation/dependency_check/cubit/dependency_check_cubit.dart';
 import 'package:inventory_v1/presentation/dependency_check/cubit/dependency_check_state.dart';
-import 'package:inventory_v1/service_locator.dart';
 
 void main() {
   late DependencyCheckCubit sut;
