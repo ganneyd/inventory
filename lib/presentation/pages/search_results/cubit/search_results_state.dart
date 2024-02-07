6c919b035f014008be448d82914d5eca64b19e4e
Ganney Dortch <ganneyd@gmail.com>
2024-02-08 02:35:07 +0300

    added new statues and list to hold parts

--- lib/presentation/pages/search_results/cubit/search_results_state.dart
+++ lib/presentation/pages/search_results/cubit/search_results_state.dart
@@ -7,16 +7,24 @@ part 'search_results_state.freezed.dart';
 //the different states the add_part page can be in
 enum SearchResultsStateStatus {
   loading,
-  searching,
   loadedSuccessfully,
   loadedUnsuccessfully,
+  searchUnsuccessful,
+  searchSuccessful,
+  searchNotFound,
+  textFieldEmpty,
+  textFieldNotEmpty,
 }
 
 @freezed
 class SearchResultsState with _$SearchResultsState {
   factory SearchResultsState({
     required ScrollController scrollController,
-    @Default(<Part>[]) List<Part> parts,
+    @Default(<Part>[]) List<Part> partsByName,
+    @Default(<Part>[]) List<Part> partsByNsn,
+    @Default(<Part>[]) List<Part> partsBySerialNumber,
+    @Default(<Part>[]) List<Part> partsByPartNumber,
+    required TextEditingController searchBarController,
     @Default('no-error') String error,
     @Default(SearchResultsStateStatus.loading) SearchResultsStateStatus status,
   }) = _SearchResultsState;
