fdd772af40451f926f95dcc858f8bb429d69d736
Ganney Dortch <ganneyd@gmail.com>
2024-02-08 02:27:32 +0300

    refactored to return a part list instead of entity and added debugging comments

--- lib/domain/usecases/get_part_by_serial_number.dart
+++ lib/domain/usecases/get_part_by_serial_number.dart
@@ -3,24 +3,30 @@ import 'package:equatable/equatable.dart';
 import 'package:inventory_v1/core/error/failures.dart';
 import 'package:inventory_v1/core/usecases/usecases.dart';
 import 'package:inventory_v1/data/entities/part/part_entity.dart';
+import 'package:inventory_v1/domain/models/part/part_model.dart';
 import 'package:inventory_v1/domain/repositories/part_repository.dart';
+import 'package:logging/logging.dart';
 
 class GetPartBySerialNumberUsecase
-    implements UseCase<List<PartEntity>, GetAllPartBySerialNumberParams> {
-  const GetPartBySerialNumberUsecase(PartRepository partRepository)
-      : _partRepository = partRepository;
+    implements UseCase<List<Part>, GetAllPartBySerialNumberParams> {
+  GetPartBySerialNumberUsecase(PartRepository partRepository)
+      : _partRepository = partRepository,
+        _logger = Logger('get-part-by-serial-number-usecase');
 
   final PartRepository _partRepository;
+  final Logger _logger;
   @override
-  Future<Either<Failure, List<PartEntity>>> call(
+  Future<Either<Failure, List<Part>>> call(
       GetAllPartBySerialNumberParams params) async {
+    _logger.finest('evoking call to part repo ${PartField.serialNumber}');
     Either<Failure, List<PartEntity>> usecase =
         await _partRepository.searchPartsByField(
             fieldName: PartField.serialNumber, queryKey: params.queryKey);
 
     return usecase.fold(
-        (l) => const Left<Failure, List<PartEntity>>(ReadDataFailure()),
-        (List<PartEntity> parts) => Right<Failure, List<PartEntity>>(parts));
+        (l) => const Left<Failure, List<Part>>(ReadDataFailure()),
+        (List<PartEntity> parts) =>
+            Right<Failure, List<Part>>(_partRepository.toPartList(parts)));
   }
 }
 
