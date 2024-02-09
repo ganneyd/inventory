e44b4c1eec5313768114401d4e45fbb797e147ed
Ganney Dortch <ganneyd@gmail.com>
2024-02-10 00:09:04 +0300

    added update functionality to quantity

--- lib/presentation/pages/search_results/view/add_to_cart_dialog.dart
+++ lib/presentation/pages/search_results/view/add_to_cart_dialog.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:inventory_v1/data/entities/part/part_entity.dart';
+import 'package:inventory_v1/presentation/utils/number_input_formatter.dart';
 
 class AddToCartDialog extends StatelessWidget {
   final PartEntity part;
+
   final void Function(int quantity) onAdd;
 
   const AddToCartDialog({
@@ -12,38 +14,80 @@ class AddToCartDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    int quantity = 1;
+    final TextEditingController controller =
+        TextEditingController(text: '$quantity');
     return AlertDialog(
-      title: const Text('Add to Cart'),
-      content: SingleChildScrollView(
-        child: ListBody(
-          children: <Widget>[
-            Text('Specify the quantity for ${part.name}:'),
-            TextField(
-              autofocus: true,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(labelText: 'Quantity'),
-              onSubmitted: (value) {
-                final int quantity = int.tryParse(value) ?? 1;
+      title: const Center(child: Text('Add to Cart')),
+      content: StatefulBuilder(
+        builder: (BuildContext context, StateSetter setState) {
+          return Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const Text('Specify the quantity for'),
+              Text(
+                "${part.name}: $quantity",
+                style:
+                    const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                      onPressed: quantity > 1
+                          ? () => setState(() {
+                                quantity--;
+                                controller.text = quantity.toString();
+                              })
+                          : null,
+                      icon: const Icon(Icons.minimize)),
+                  SizedBox(
+                    width: 20,
+                    child: TextField(
+                        inputFormatters: [PositiveNumberInputFormatter()],
+                        controller: controller,
+                        onChanged: (value) {
+                          var updatedQuantity = int.tryParse(value) ??
+                              1; // Update the quantity based on input}
+                          if (updatedQuantity <= part.quantity) {
+                            // Use the 'setState' provided by StatefulBuilder to update the quantity
+                            setState(() {
+                              quantity = updatedQuantity;
+                            });
+                          }
+                        }),
+                  ),
+                  IconButton(
+                      onPressed: quantity < part.quantity
+                          ? () => setState(() {
+                                quantity++;
+                                controller.text = quantity.toString();
+                              })
+                          : null,
+                      icon: const Icon(Icons.add)),
+                ],
+              ),
+            ],
+          );
+        },
+      ),
+      actions: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () => Navigator.of(context).pop(), // Close the dialog
+            ),
+            TextButton(
+              child: const Text('Add'),
+              onPressed: () {
+                // Use the local 'quantity' variable when 'Add' is pressed
                 onAdd(quantity);
                 Navigator.of(context).pop(); // Close the dialog
               },
             ),
           ],
-        ),
-      ),
-      actions: <Widget>[
-        TextButton(
-          child: const Text('Cancel'),
-          onPressed: () => Navigator.of(context).pop(), // Close the dialog
-        ),
-        TextButton(
-          child: const Text('Add'),
-          onPressed: () {
-            // You can also include logic here to fetch the quantity from the TextField
-            // For simplicity, we're just calling the onAdd function with a default value
-            onAdd(1); // Use a proper way to get the quantity from the TextField
-            Navigator.of(context).pop(); // Close the dialog
-          },
         ),
       ],
     );
