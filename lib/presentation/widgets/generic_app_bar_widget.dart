3e263b6b359000ed082f9e4718f159a0d7d73423
Ganney Dortch <ganneyd@gmail.com>
2024-02-09 03:32:17 +0300

    refactored to be a widget

--- lib/presentation/widgets/generic_app_bar_widget.dart
+++ lib/presentation/widgets/generic_app_bar_widget.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
 
-AppBar genericAppBar(BuildContext context, String title) {
-  return AppBar(
-      title: Text(title),
-      leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () => Navigator.of(context).pop()));
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final List<Widget>? actions;
+  final PreferredSizeWidget? bottom;
+  const CustomAppBar(
+      {required Key key, required this.title, this.actions, this.bottom})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+        bottom: bottom,
+        title: Center(child: Text(title)),
+        actions: actions,
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () => Navigator.of(context).pop(),
+            );
+          },
+        ));
+  }
+
+  @override
+  Size get preferredSize {
+    // Here, we provide the preferred size of the AppBar.
+    // The default AppBar height is 56.0, and we add the bottom widget's preferred height if it exists.
+    return Size.fromHeight(
+      kToolbarHeight + (bottom?.preferredSize.height ?? 0.0),
+    );
+  }
 }
