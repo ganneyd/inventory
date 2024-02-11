3f3020aa40f7e92a6484ada06fdf97f0bbcae3ee
Ganney Dortch <ganneyd@gmail.com>
2024-02-12 02:50:04 +0300

    added new usecases

--- lib/presentation/pages/manage_inventory/cubit/manage_inventory_cubit.dart
+++ lib/presentation/pages/manage_inventory/cubit/manage_inventory_cubit.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:inventory_v1/core/usecases/usecases.dart';
+import 'package:inventory_v1/data/entities/checked-out/checked_out_entity.dart';
 import 'package:inventory_v1/data/entities/part/part_entity.dart';
 import 'package:inventory_v1/domain/usecases/usecases_bucket.dart';
 import 'package:inventory_v1/presentation/pages/manage_inventory/cubit/manage_inventory_state.dart';
@@ -11,15 +12,21 @@ class ManageInventoryCubit extends Cubit<ManageInventoryState> {
       {int fetchPartAmount = 20,
       required this.getAllPartsUsecase,
       required this.getDatabaseLength,
+      required this.getUnverifiedCheckoutParts,
+      required this.getAllCheckoutParts,
+      required this.getLowQuantityParts,
+      required this.verifyCheckoutPartUsecase,
       required ScrollController scrollController})
       : _logger = Logger('manage-inv-cubit'),
-        super(ManageInventoryState(
-            scrollController: scrollController,
-            fetchPartAmount: fetchPartAmount));
+        super(ManageInventoryState(fetchPartAmount: fetchPartAmount));
 
   //usecase init
   final GetAllPartsUsecase getAllPartsUsecase;
+  final GetLowQuantityParts getLowQuantityParts;
   final GetDatabaseLength getDatabaseLength;
+  final GetUnverifiedCheckoutParts getUnverifiedCheckoutParts;
+  final VerifyCheckoutPart verifyCheckoutPartUsecase;
+  final GetAllCheckoutParts getAllCheckoutParts;
   //for debugging
   final Logger _logger;
 
@@ -67,27 +74,86 @@ class ManageInventoryCubit extends Cubit<ManageInventoryState> {
         (l) => emit(state.copyWith(databaseLength: 0)),
         (r) => emit(state.copyWith(
             databaseLength: r, status: ManageInventoryStateStatus.loading)));
-
     loadParts();
-    //initialize the scroll controller callback function
-    state.scrollController.addListener(_fetchMoreParts);
+    loadCheckedOutParts();
   }
 
-  //method to fetch more parts when the user scrolls to the end of the list
-  void _fetchMoreParts() async {
-    //check if user is actually at the end
-    if (state.scrollController.position.pixels ==
-            state.scrollController.position.maxScrollExtent &&
-        state.parts.length < state.databaseLength) {
-      emit(state.copyWith(status: ManageInventoryStateStatus.fetchingData));
-      loadParts();
+  Future<void> loadCheckedOutParts() async {
+    // Indicate loading state, especially useful for UI indicators
+    emit(state.copyWith(status: ManageInventoryStateStatus.loading));
+
+    var startIndex = state.checkedOutParts.length;
+    var endIndex = startIndex + state.fetchPartAmount;
+    List<CheckedOutEntity> oldCheckoutPartList = state.checkedOutParts.toList();
+
+    _logger.finest(
+        'loading checked out parts, startIndex:$startIndex endIndex:$endIndex');
+
+    var results = await getAllCheckoutParts.call(
+        GetAllCheckoutPartsParams(endIndex: endIndex, startIndex: startIndex));
+
+    results.fold((failure) {
+      _logger.warning('error while retrieving checked out parts');
+      emit(state.copyWith(
+          error: failure.errorMessage,
+          status: ManageInventoryStateStatus.fetchedDataUnsuccessfully));
+    }, (newParts) {
+      _logger.finest('retrieved all checked out parts: ${newParts.length}');
+      oldCheckoutPartList.addAll(newParts); // Use addAll for efficiency
+
+      emit(state.copyWith(
+          status: ManageInventoryStateStatus.fetchedDataSuccessfully,
+          checkedOutParts: oldCheckoutPartList));
+      _logger.finest(
+          'state has ${state.checkedOutParts.length} checked out parts');
+    });
+  }
+
+  void loadUnverifiedParts({bool verifiedPartSuccessfully = false}) async {
+    // Assuming loadCheckedOutParts() updates state.checkedOutParts correctly
+    await loadCheckedOutParts();
+
+    // Efficiently filter out unverified parts
+    var newUnverifiedList = state.checkedOutParts
+        .where((part) => !(part.isVerified ?? true))
+        .toList();
+
+    emit(state.copyWith(
+      unverifiedParts: newUnverifiedList,
+      // Update the status only if verification was successful
+      status: verifiedPartSuccessfully
+          ? ManageInventoryStateStatus.verifiedPartSuccessfully
+          : state.status,
+    ));
+
+    _logger
+        .finest('state has ${state.unverifiedParts.length} unverified parts');
+  }
+
+  void loadLowQuantityParts() async {
+    loadParts();
+    List<PartEntity> newLowQuantityList = [];
+    for (var part in state.parts) {
+      if (part.quantity <= part.requisitionPoint) {
+        newLowQuantityList.add(part);
+      }
     }
+    emit(state.copyWith(lowQuantityParts: newLowQuantityList));
   }
 
-//ensures the scroll controller is properly disposed of
-  @override
-  Future<void> close() {
-    state.scrollController.dispose();
-    return super.close();
+  void verifyCheckoutPart(CheckedOutEntity checkoutPart) async {
+    emit(state.copyWith(status: ManageInventoryStateStatus.verifyingPart));
+
+    var results = await verifyCheckoutPartUsecase
+        .call(VerifyCheckoutPartParams(checkedOutEntity: checkoutPart));
+
+    results.fold(
+        (fail) => emit(state.copyWith(
+            error: fail.errorMessage,
+            status: ManageInventoryStateStatus.verifiedPartUnsuccessfully)),
+        (_) {
+      // Directly move to loading unverified parts without clearing the lists
+      loadUnverifiedParts(verifiedPartSuccessfully: true);
+    });
   }
 }
