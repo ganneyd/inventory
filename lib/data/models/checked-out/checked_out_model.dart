da025ffb15256097cb30584b3d72bacde037d3a6
Ganney Dortch <ganneyd@gmail.com>
2024-02-12 02:45:49 +0300

    refactored to use a partEntity

--- lib/data/models/checked-out/checked_out_model.dart
+++ lib/data/models/checked-out/checked_out_model.dart
@@ -1,5 +1,6 @@
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:inventory_v1/data/entities/checked-out/checked_out_entity.dart';
+import 'package:inventory_v1/data/entities/part/part_entity.dart';
 import 'package:inventory_v1/data/models/part/part_model.dart';
 
 part 'checked_out_model.g.dart';
