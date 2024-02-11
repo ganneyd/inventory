8823615a0c99699f76925ffa412f1f9b53fc06a8
Ganney Dortch <ganneyd@gmail.com>
2024-02-12 02:47:49 +0300

    added debug

--- lib/domain/usecases/checkout/verify_checkout_part.dart
+++ lib/domain/usecases/checkout/verify_checkout_part.dart
@@ -4,15 +4,19 @@ import 'package:inventory_v1/core/error/failures.dart';
 import 'package:inventory_v1/core/usecases/usecases.dart';
 import 'package:inventory_v1/data/entities/checked-out/checked_out_entity.dart';
 import 'package:inventory_v1/domain/repositories/checked_out_part_repository.dart';
+import 'package:logging/logging.dart';
 
 class VerifyCheckoutPart implements UseCase<void, VerifyCheckoutPartParams> {
-  const VerifyCheckoutPart(CheckedOutPartRepository checkedOutPartRepository)
-      : _checkedOutPartRepository = checkedOutPartRepository;
+  VerifyCheckoutPart(CheckedOutPartRepository checkedOutPartRepository)
+      : _checkedOutPartRepository = checkedOutPartRepository,
+        _logger = Logger('verify-part-usecase');
   final CheckedOutPartRepository _checkedOutPartRepository;
+  final Logger _logger;
   @override
   Future<Either<Failure, void>> call(VerifyCheckoutPartParams params) async {
     var date = DateTime.now();
     var updatedCheckoutPart = CheckedOutEntity(
+        index: params.checkedOutEntity.index,
         checkedOutQuantity: params.checkedOutEntity.checkedOutQuantity,
         dateTime: params.checkedOutEntity.dateTime,
         part: params.checkedOutEntity.part,
@@ -24,9 +28,10 @@ class VerifyCheckoutPart implements UseCase<void, VerifyCheckoutPartParams> {
           date.hour,
         ));
 
-    var results =
-        await _checkedOutPartRepository.editCheckedOutItem(updatedCheckoutPart);
-
+    var results = await _checkedOutPartRepository.editCheckedOutItem(
+      updatedCheckoutPart,
+    );
+    _logger.finest('verified $updatedCheckoutPart');
     return results.fold((failure) => Left<Failure, void>(failure),
         (r) => const Right<Failure, void>(null));
   }
