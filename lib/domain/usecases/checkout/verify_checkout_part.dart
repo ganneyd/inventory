c1b0517ea5cc4b26a5f2d2f8f7cb129272ed3a96
Ganney Dortch <ganneyd@gmail.com>
2024-02-15 18:15:18 +0300

    ensured part was also being updated when a checkout part is verified

--- lib/domain/usecases/checkout/verify_checkout_part.dart
+++ lib/domain/usecases/checkout/verify_checkout_part.dart
@@ -2,45 +2,53 @@ import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 import 'package:inventory_v1/core/error/failures.dart';
 import 'package:inventory_v1/core/usecases/usecases.dart';
-import 'package:inventory_v1/data/entities/checked-out/checked_out_entity.dart';
+import 'package:inventory_v1/domain/entities/checked-out/checked_out_entity.dart';
+import 'package:inventory_v1/domain/entities/part/part_entity.dart';
 import 'package:inventory_v1/domain/repositories/checked_out_part_repository.dart';
+import 'package:inventory_v1/domain/usecases/edit_part.dart';
 import 'package:logging/logging.dart';
 
 class VerifyCheckoutPart implements UseCase<void, VerifyCheckoutPartParams> {
-  VerifyCheckoutPart(CheckedOutPartRepository checkedOutPartRepository)
+  VerifyCheckoutPart(CheckedOutPartRepository checkedOutPartRepository,
+      EditPartUsecase editPartUsecase)
       : _checkedOutPartRepository = checkedOutPartRepository,
+        _editPartUsecase = editPartUsecase,
         _logger = Logger('verify-part-usecase');
   final CheckedOutPartRepository _checkedOutPartRepository;
+  final EditPartUsecase _editPartUsecase;
   final Logger _logger;
+
   @override
   Future<Either<Failure, void>> call(VerifyCheckoutPartParams params) async {
-    var date = DateTime.now();
-    var updatedCheckoutPart = CheckedOutEntity(
-        index: params.checkedOutEntity.index,
-        checkedOutQuantity: params.checkedOutEntity.checkedOutQuantity,
-        dateTime: params.checkedOutEntity.dateTime,
-        part: params.checkedOutEntity.part,
-        isVerified: true,
-        verifiedDate: DateTime(
-          date.year,
-          date.month,
-          date.day,
-          date.hour,
-        ));
+    for (var checkoutPart in params.checkedOutEntityList) {
+      PartEntity updatedPart = checkoutPart.part;
 
-    var results = await _checkedOutPartRepository.editCheckedOutItem(
-      updatedCheckoutPart,
-    );
-    _logger.finest('verified $updatedCheckoutPart');
-    return results.fold((failure) => Left<Failure, void>(failure),
-        (r) => const Right<Failure, void>(null));
+      var updatePartResults =
+          await _editPartUsecase.call(EditPartParams(partEntity: updatedPart));
+      if (updatePartResults.isLeft()) {
+        _logger.warning('error encounter when updating part');
+        return const Left<Failure, void>(UpdateDataFailure());
+      }
+
+      var updatedCheckoutPart =
+          checkoutPart.copyWith(verifiedDate: DateTime.now(), isVerified: true);
+      var result = await _checkedOutPartRepository
+          .editCheckedOutItem(updatedCheckoutPart);
+      if (result.isLeft()) {
+        _logger.warning('error encounter when updating check out part');
+        return const Left<Failure, void>(UpdateDataFailure());
+      }
+    }
+
+    return const Right<Failure, void>(null);
   }
 }
 
 class VerifyCheckoutPartParams extends Equatable {
-  const VerifyCheckoutPartParams({required this.checkedOutEntity});
-  final CheckedOutEntity checkedOutEntity;
-
+  const VerifyCheckoutPartParams({
+    required this.checkedOutEntityList,
+  });
+  final List<CheckedOutEntity> checkedOutEntityList;
   @override
-  List<Object?> get props => [checkedOutEntity];
+  List<Object?> get props => [checkedOutEntityList];
 }
