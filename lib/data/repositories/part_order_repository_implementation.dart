36a41e508c8f6c0482d1fb86441733bdc3588636
Ganney Dortch <ganneyd@gmail.com>
2024-02-20 19:21:34 +0300

    added checks before deleting data and use key instead of auto-increment index

--- lib/data/repositories/part_order_repository_implementation.dart
+++ lib/data/repositories/part_order_repository_implementation.dart
@@ -17,11 +17,12 @@ class PartOrderRepositoryImplementation extends PartOrderRepository {
   @override
   Future<Either<Failure, void>> createPartOrder(OrderEntity orderEntity) async {
     try {
-      var index = _localDatasource.isEmpty ? 0 : _localDatasource.length;
-      _logger.finest('added $orderEntity to database');
-      await _localDatasource.add(orderEntity.copyWith(index: index).toModel());
+      await _localDatasource.put(orderEntity.index, orderEntity.toModel());
+      _logger.finest(
+          'added $orderEntity index: ${orderEntity.index} to database database length is ${_localDatasource.length}');
       return const Right<Failure, void>(null);
     } catch (e) {
+      _logger.warning('exception occurred ${e.toString()}');
       return const Left<Failure, void>(CreateDataFailure());
     }
   }
@@ -29,8 +30,16 @@ class PartOrderRepositoryImplementation extends PartOrderRepository {
   @override
   Future<Either<Failure, void>> deletePartOrder(OrderEntity orderEntity) async {
     try {
-      await _localDatasource.deleteAt(orderEntity.index);
-      return const Right<Failure, void>(null);
+      _logger.finest('deleting ${orderEntity.index}  ');
+
+      if (_localDatasource.containsKey(orderEntity.index)) {
+        await _localDatasource.delete(orderEntity.index);
+
+        return const Right<Failure, void>(null);
+      }
+      _logger.warning(
+          'box does not contain key ${orderEntity.index} only ${_localDatasource.keys}');
+      throw Exception();
     } catch (e) {
       return const Left<Failure, void>(DeleteDataFailure());
     }
@@ -40,6 +49,8 @@ class PartOrderRepositoryImplementation extends PartOrderRepository {
   Future<Either<Failure, void>> editPartOrder(OrderEntity orderEntity) async {
     try {
       await _localDatasource.put(orderEntity.index, orderEntity.toModel());
+      _logger.finest(
+          'edited ${orderEntity.index} with fulfillment date ${orderEntity.fulfillmentDate}');
       return const Right<Failure, void>(null);
     } catch (e) {
       return const Left<Failure, void>(UpdateDataFailure());
@@ -50,20 +61,33 @@ class PartOrderRepositoryImplementation extends PartOrderRepository {
   Future<Either<Failure, List<OrderEntity>>> getAllPartOrders(
       int startIndex, int endIndex) async {
     try {
-      if (startIndex >= _localDatasource.length) {
+      _logger.finest(endIndex);
+      var end = _localDatasource.length + endIndex;
+      var upperBound = _localDatasource.length - 1 - startIndex;
+      var lowerBound = 0 > end ? 0 : end;
+
+      _logger.finest(
+          'getting all parts lowerBound is $lowerBound and upperBound is $upperBound');
+      if (upperBound == -1) {
         return const Right<Failure, List<OrderEntity>>([]);
       }
-      if (startIndex >= endIndex ||
-          startIndex.isNegative ||
-          endIndex.isNegative) {
-        throw IndexOutOfBounds;
+      if (lowerBound > upperBound) {
+        _logger.severe(
+            'lowerBound is greater than upperBound, throwing exception... ');
+        throw IndexOutOfBounds();
       }
-      var orderList = _localDatasource.valuesBetween(
-          startKey: startIndex, endKey: endIndex);
-      var updatedIndexList = orderList
-          .map((order) => order.copyWith(index: startIndex++))
-          .toList();
-      return Right<Failure, List<OrderEntity>>(updatedIndexList);
+
+      List<OrderEntity> orderList = [];
+      for (int i = upperBound; i >= lowerBound; i--) {
+        var order = _localDatasource.getAt(i);
+        var key = _localDatasource.keyAt(i);
+        if (order != null) {
+          orderList.add(order.copyWith(index: key));
+        }
+      }
+      _logger.finest('got ${orderList.length} parts from database');
+
+      return Right<Failure, List<OrderEntity>>(orderList.toList());
     } catch (e) {
       return const Left<Failure, List<OrderEntity>>(ReadDataFailure());
     }
