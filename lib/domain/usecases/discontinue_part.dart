03e0995f996f7ad235395e3830a8c7d8673684aa
Ganney Dortch <ganneyd@gmail.com>
2024-02-21 03:30:14 +0300

    added debugging

--- lib/domain/usecases/discontinue_part.dart
+++ lib/domain/usecases/discontinue_part.dart
@@ -5,14 +5,17 @@ import 'package:inventory_v1/core/usecases/usecases.dart';
 import 'package:inventory_v1/domain/entities/part/part_entity.dart';
 import 'package:inventory_v1/domain/repositories/part_order_repository.dart';
 import 'package:inventory_v1/domain/repositories/part_repository.dart';
+import 'package:logging/logging.dart';
 
 class DiscontinuePartUsecase implements UseCase<void, DiscontinuePartParams> {
-  const DiscontinuePartUsecase(
+  DiscontinuePartUsecase(
       PartOrderRepository partOrderRepository, PartRepository partRepository)
       : _partOrderRepository = partOrderRepository,
-        _partRepository = partRepository;
+        _partRepository = partRepository,
+        _logger = Logger('discontinue-part-usecase');
   final PartRepository _partRepository;
   final PartOrderRepository _partOrderRepository;
+  final Logger _logger;
   @override
   Future<Either<Failure, void>> call(DiscontinuePartParams params) async {
     bool success = true;
@@ -22,8 +25,11 @@ class DiscontinuePartUsecase implements UseCase<void, DiscontinuePartParams> {
       var results = await _partRepository.editPart(
           params.discontinuedPartEntity.copyWith(isDiscontinued: true));
       if (results.isLeft()) {
+        _logger.warning(
+            'failure encounter while editing  part ${params.discontinuedPartEntity.index}');
         return const Left<Failure, void>(UpdateDataFailure());
       }
+      _logger.finest('edited part successfully');
       var orderList = orderListResults.getOrElse(() => []);
 
       for (var order in orderList) {
@@ -32,9 +38,14 @@ class DiscontinuePartUsecase implements UseCase<void, DiscontinuePartParams> {
       }
 
       if (success) {
+        _logger.finest('deleted all orders related to list');
         return const Right<Failure, void>(null);
       }
+      _logger.warning(
+          'failure encounter while deleting orders ${orderList.length}');
     }
+    _logger.warning(
+        'failure encounter while retrieving order list for part ${params.discontinuedPartEntity.index}');
     return const Left<Failure, void>(ReadDataFailure());
   }
 }
