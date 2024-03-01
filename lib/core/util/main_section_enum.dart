84c7379e176efef2e5057add2fdb88b5b6b29f82
Ganney Dortch <ganneyd@gmail.com>
2024-03-01 20:40:19 +0300

    created enum to represent the various maintenance sections

--- lib/core/util/main_section_enum.dart
+++ lib/core/util/main_section_enum.dart
@@ -1,7 +1,7 @@
 import 'package:hive/hive.dart';
 part 'main_section_enum.g.dart';
 
-@HiveType(typeId: 4)
+@HiveType(typeId: 5)
 enum MaintenanceSection {
   @HiveField(0)
   uh,
@@ -11,4 +11,23 @@ enum MaintenanceSection {
   aH,
   @HiveField(3)
   civ,
+  @HiveField(4)
+  unknown
+}
+
+extension MaintenanceSectionExtension on MaintenanceSection {
+  String enumToString() {
+    switch (this) {
+      case MaintenanceSection.aH:
+        return '64 Maint';
+      case MaintenanceSection.uh:
+        return '60 Maint';
+      case MaintenanceSection.chM:
+        return '47 Maint';
+      case MaintenanceSection.civ:
+        return 'Civilian';
+      default:
+        return 'Not Sure';
+    }
+  }
 }
