6506dd88a9e673a0f7c2c6420ef3a24895da9cb2
Ganney Dortch <ganneyd@gmail.com>
2024-03-01 17:59:10 +0300

    refactored dialog to be stateful widget

--- lib/presentation/pages/search_results/view/add_to_cart_dialog.dart
+++ lib/presentation/pages/search_results/view/add_to_cart_dialog.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:inventory_v1/domain/entities/part/part_entity.dart';
 import 'package:inventory_v1/presentation/utils/number_input_formatter.dart';
 
-class AddToCartDialog extends StatelessWidget {
+class AddToCartDialog extends StatefulWidget {
   final PartEntity part;
 
   final void Function(int quantity) onAdd;
@@ -13,63 +13,90 @@ class AddToCartDialog extends StatelessWidget {
   }) : super(key: const Key('alert-dialog'));
 
   @override
+  State<AddToCartDialog> createState() => _AddToCartDialogState();
+}
+
+class _AddToCartDialogState extends State<AddToCartDialog> {
+  late FocusNode _focusNode;
+
+  int quantity = 1;
+  late TextEditingController controller;
+  @override
+  void initState() {
+    controller = TextEditingController(text: '$quantity');
+    _focusNode = FocusNode();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    int quantity = 1;
-    final TextEditingController controller =
-        TextEditingController(text: '$quantity');
     return AlertDialog(
       title: const Center(child: Text('Add to Cart')),
-      content: StatefulBuilder(
-        builder: (BuildContext context, StateSetter setState) {
-          return Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              const Text('Specify the quantity for'),
-              Text(
-                "${part.name}: $quantity",
-                style:
-                    const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  IconButton(
-                      onPressed: quantity > 1
-                          ? () => setState(() {
-                                quantity--;
-                                controller.text = quantity.toString();
-                              })
-                          : null,
-                      icon: const Icon(Icons.minimize)),
-                  SizedBox(
-                    width: 20,
-                    child: TextField(
-                        inputFormatters: [PositiveNumberInputFormatter()],
-                        controller: controller,
-                        onChanged: (value) {
-                          var updatedQuantity = int.tryParse(value) ??
-                              1; // Update the quantity based on input}
-                          if (updatedQuantity <= part.quantity) {
-                            // Use the 'setState' provided by StatefulBuilder to update the quantity
-                            setState(() {
-                              quantity = updatedQuantity;
-                            });
-                          }
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Text('Specify the quantity for'),
+          Text(
+            "${widget.part.name}: $quantity",
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                  onPressed: quantity > 1
+                      ? () => setState(() {
+                            quantity--;
+                            controller.text = quantity.toString();
+                          })
+                      : null,
+                  icon: const Icon(Icons.minimize)),
+              SizedBox(
+                width: 20,
+                child: TextField(
+                    focusNode: _focusNode,
+                    inputFormatters: [PositiveNumberInputFormatter()],
+                    controller: controller,
+                    onTapOutside: (event) => setState(() {
+                          controller.text = quantity.toString();
                         }),
-                  ),
-                  IconButton(
-                      onPressed: quantity < part.quantity
-                          ? () => setState(() {
-                                quantity++;
-                                controller.text = quantity.toString();
-                              })
-                          : null,
-                      icon: const Icon(Icons.add)),
-                ],
+                    onEditingComplete: () => setState(() {
+                          controller.text = quantity.toString();
+                        }),
+                    onChanged: (value) {
+                      var updatedQuantity = int.tryParse(value) ??
+                          1; // Update the quantity based on input}
+                      if (updatedQuantity <= widget.part.quantity) {
+                        // Use the 'setState' provided by StatefulBuilder to update the quantity
+                        setState(() {
+                          quantity = updatedQuantity;
+                        });
+                      } else {
+                        setState(() {
+                          quantity = widget.part.quantity;
+                        });
+                      }
+                    }),
               ),
+              IconButton(
+                  onPressed: quantity < widget.part.quantity
+                      ? () => setState(() {
+                            quantity++;
+                            controller.text = quantity.toString();
+                          })
+                      : null,
+                  icon: const Icon(Icons.add)),
             ],
-          );
-        },
+          ),
+        ],
       ),
       actions: <Widget>[
         Row(
@@ -83,7 +110,7 @@ class AddToCartDialog extends StatelessWidget {
               child: const Text('Add'),
               onPressed: () {
                 // Use the local 'quantity' variable when 'Add' is pressed
-                onAdd(quantity);
+                widget.onAdd(quantity);
                 Navigator.of(context).pop(); // Close the dialog
               },
             ),
