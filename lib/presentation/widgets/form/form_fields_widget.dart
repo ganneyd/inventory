963c54f24057e7ce2e425fbd491e07a6548b05b9
Ganney Dortch <ganneyd@gmail.com>
2024-03-19 04:04:05 +0300

    refactored to capitalized text automatically

--- lib/presentation/widgets/form/form_fields_widget.dart
+++ lib/presentation/widgets/form/form_fields_widget.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:inventory_v1/presentation/utils/utils_bucket.dart';
 
-class CustomTextField extends StatelessWidget {
+class CustomTextField extends StatefulWidget {
   final TextEditingController controller;
   final String hintText;
   final String? Function(String?)? validation;
@@ -21,32 +21,60 @@ class CustomTextField extends StatelessWidget {
       this.inputFormatter,
       this.isNumberInput = false,
       this.maxLength});
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  @override
+  void initState() {
+    widget.controller.addListener(() {
+      final text = widget.controller.text.toUpperCase();
+      if (text != widget.controller.text) {
+        widget.controller.value = TextEditingValue(
+          text: text,
+          selection: TextSelection.collapsed(offset: text.length),
+        );
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    //widget.controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      focusNode: focusNode,
-      maxLength: maxLength,
-      inputFormatters:
-          isNumberInput ? [PositiveNumberInputFormatter()] : inputFormatter,
-      validator: validation ??
+      textCapitalization: TextCapitalization.characters,
+      focusNode: widget.focusNode,
+      maxLength: widget.maxLength,
+      inputFormatters: widget.isNumberInput
+          ? [PositiveNumberInputFormatter()]
+          : widget.inputFormatter,
+      validator: widget.validation ??
           (value) {
-            if (isNumberInput) {
-              return validatePositiveNumber(value, numberGreaterThan);
-            } else if (maxLength != null && value != null) {
-              if (value.length != maxLength) {
+            if (widget.isNumberInput) {
+              return validatePositiveNumber(value, widget.numberGreaterThan);
+            } else if (widget.maxLength != null && value != null) {
+              if (value.length != widget.maxLength) {
                 return 'Please enter a correct NSN';
               } else {
                 return null;
               }
             } else if (value == null || value.isEmpty) {
-              return 'Please enter a $hintText.';
+              return 'Please enter a ${widget.hintText}.';
             }
             return null;
           },
-      controller: controller,
+      controller: widget.controller,
       decoration: InputDecoration(
           counterText: '',
-          hintText: hintText,
+          hintText: widget.hintText,
           border: const OutlineInputBorder()),
     );
   }
