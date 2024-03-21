cba714086884deb465cf15c9da36031468a4dbd1
Ganney Dortch <ganneyd@gmail.com>
2024-03-21 03:41:49 +0300

    if no user exists then the first user to be created is granted all rights

--- lib/domain/usecases/authentication/create_user_usecase.dart
+++ lib/domain/usecases/authentication/create_user_usecase.dart
@@ -3,6 +3,7 @@ import 'package:equatable/equatable.dart';
 import 'package:inventory_v1/core/error/failures.dart';
 import 'package:inventory_v1/core/usecases/usecases.dart';
 import 'package:inventory_v1/core/util/rank_enum.dart';
+import 'package:inventory_v1/core/util/view_right_enum.dart';
 import 'package:inventory_v1/domain/entities/user/user_entity.dart';
 import 'package:inventory_v1/domain/repositories/authentication_repository.dart';
 import 'package:logging/logging.dart';
@@ -17,9 +18,20 @@ class CreateUserUsecase implements UseCase<void, CreateUserParams> {
 
   @override
   Future<Either<Failure, void>> call(CreateUserParams params) async {
+    var results = await _authenticationRepository.getUsers();
+    var users = results.getOrElse(() => [params.userEntity]);
+
     var updatedUser = params.userEntity.copyWith(
         username:
             '${params.userEntity.firstName}.${params.userEntity.lastName}');
+    if (users.isEmpty) {
+      updatedUser = updatedUser.copyWith(viewRights: [
+        ViewRightsEnum.admin,
+        ViewRightsEnum.orders,
+        ViewRightsEnum.parts,
+        ViewRightsEnum.verify
+      ]);
+    }
     _logger.finest(
         'updated username for ${params.userEntity.rank.enumToString()}- ${params.userEntity.lastName} to ${updatedUser.username}');
     return await _authenticationRepository.createUser(updatedUser);
